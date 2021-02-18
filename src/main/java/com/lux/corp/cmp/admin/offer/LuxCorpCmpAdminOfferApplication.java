c08ad85a96ebd4f6464fac66f78f0cb2ede3ca37
fadrea082 <fabio.real@pichincha.com.co>
2021-02-18 18:22:03 -0500

    Creation project admin offer basic

---
+++ src/main/java/com/lux/corp/cmp/admin/offer/LuxCorpCmpAdminOfferApplication.java
@@ -0,0 +1,13 @@
+package com.lux.corp.cmp.admin.offer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxCorpCmpAdminOfferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxCorpCmpAdminOfferApplication.class, args);
+	}
+
+}
